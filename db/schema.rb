63f8e9eb6fe49dce3db3fc2e4fbae2f569748bfb
enrsrowe <S.Rowell@2010.ljmu.ac.uk>
2013-04-22 11:13:57 +0100

    Updated Everything
    Everything is working after multiple recommits. Silly ubuntu.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130415175846) do
+ActiveRecord::Schema.define(:version => 20130421212941) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(:version => 20130415175846) do
     t.string   "title"
     t.text     "body"
     t.string   "date"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "my_flickrs", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
